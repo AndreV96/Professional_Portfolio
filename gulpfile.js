40ba5a2fa46794930cf2525c37849c8ba6b576d6
AndreV96 <91210772+AndreV96@users.noreply.github.com>
2021-11-10 17:40:14 -0600

    Added my own template for webpages, it is one that I created before starting the bootcamp so I could webpages that incorporate SASS, JS, and used Gulp to incorporate all JS and styles pages in one

---
+++ gulpfile.js
@@ -0,0 +1,72 @@
+const{src, dest, watch, parallel} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+
+// Imagenes
+const cache = require('gulp-cache')
+const imagemin = require('gulp-imagemin')
+const webp = require('gulp-webp')
+const avif = require('gulp-avif');
+
+function css(done) {
+    src('src/scss/**/*.scss') //IDentificar el archivo .SCSS a compilar
+        .pipe(plumber()) //Para no detener el workflow
+        .pipe(sass()) //Compilarlo
+        .pipe(dest('./build/css')) //Alñamcenarla en el disco duro
+        done();
+}
+
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
+    done();
+};
+
+function versionWebp(done) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+    done();
+}
+
+function versionAvif(done) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
+    done();
+}
+
+function javascript( done) {
+    src('src/js/**/*.js')
+        .pipe(dest('build/js'));
+
+    done();
+
+}
+
+function dev(done) {
+    watch('src/scss/**/*.scss', css);
+    watch('src/js/**/*.js', javascript);
+    done();
+}
+
+exports.css = css;
+exports.js = javascript;
+exports.imagenes = imagenes
+exports.versionWebp = versionWebp
+exports.versionAvif = versionAvif
+exports.default = parallel(imagenes, versionWebp, versionAvif, javascript, dev) ;
